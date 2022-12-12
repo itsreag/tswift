74753941d37743bd4ed10953ed6300e6b06a7082
Yang <rea.guerrero01@gmail.com>
2022-12-12 12:15:22 +0800

    main files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'gallery',
+    loadChildren: () => import('./gallery/gallery.module').then( m => m.GalleryPageModule)
+  },
+  {
+    path: 'album',
+    loadChildren: () => import('./album/album.module').then( m => m.AlbumPageModule)
+  },
+  {
+    path: 'tracks',
+    loadChildren: () => import('./tracks/tracks.module').then( m => m.TracksPageModule)
+  },
+  {
+    path: 'card-about',
+    loadChildren: () => import('./pages/card-about/card-about.module').then( m => m.CardAboutPageModule)
+  },
+  {
+    path: 'card-new-rel',
+    loadChildren: () => import('./pages/card-new-rel/card-new-rel.module').then( m => m.CardNewRelPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
